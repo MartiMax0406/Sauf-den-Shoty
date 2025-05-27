4da1698db8cee86530a013c5b6b798cba2cf5224
MartiMax0406 <martimax0406@gmail.com>
2025-05-27 13:49:22 +0200

    Karte Flippen

--- Shoty_01.py
+++ Shoty_01.py
@@ -4,6 +4,47 @@ import random
 app = Flask(__name__)
 app.secret_key = 'geheim'  # Für Sessiondaten
 
+# ⬇️ Spiellogik als eigene Funktion
+def runde():
+    spieler_namen = session['spieler_namen']
+    counter = session['counter']
+    felder = session['felder']
+    aktueller_spieler = session['aktueller_spieler']
+
+    karte = random.randint(1, 5)
+    meldung = ""
+    gezogene_karte = "rueckseite.png"
+
+    if karte == 1:
+        counter[aktueller_spieler] += 1
+        meldung = 'Du hast eine 1 gezogen!'
+        gezogene_karte = 'karte_1.png'
+    elif karte == 2:
+        counter[aktueller_spieler] += 2
+        meldung = 'Du hast eine 2 gezogen!'
+        gezogene_karte = 'karte_2.png'
+    elif karte == 3:
+        counter[aktueller_spieler] += 3
+        meldung = 'Du hast eine 3 gezogen!'
+        gezogene_karte = 'karte_3.png'
+    elif karte == 4:
+        random.shuffle(felder)
+        meldung = 'Felder neu gemischt!'
+        gezogene_karte = 'karte_4.png'
+    elif karte == 5:
+        meldung = 'Shoty! Du hast eine 5 gezogen!'
+        gezogene_karte = 'karte_5.png'
+
+    # Update Session-Daten
+    session['counter'] = counter
+    session['felder'] = felder
+    session['gezogene_karte'] = gezogene_karte
+    session['meldung'] = meldung
+
+    # Spielerwechsel
+    session['aktueller_spieler'] = (aktueller_spieler + 1) % len(spieler_namen)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -11,6 +52,7 @@ def index():
         return render_template('index.html', spieler_anzahl=spieler_anzahl)
 
     return render_template('index.html')
+
 
 @app.route('/spiel_starten', methods=['POST'])
 def spiel_starten():
@@ -23,44 +65,24 @@ def spiel_starten():
     session['felder'] = [0] * 22 + [1] * 8
     random.shuffle(session['felder'])
     session['aktueller_spieler'] = 0
+    session['gezogene_karte'] = 'rueckseite.png'
+    session['meldung'] = ''
 
     return redirect(url_for('spiel'))
 
+
 @app.route('/spiel')
 def spiel():
-    spieler_namen = session['spieler_namen']
-    counter = session['counter']
-    felder = session['felder']
-    aktueller_spieler = session['aktueller_spieler']
-
-    karte = random.randint(1, 5)
-    meldung = ""
-
-    if karte == 1:
-        counter[aktueller_spieler] += 1
-        meldung = 'Du hast eine 1 geworfen!'
-    elif karte == 2:
-        counter[aktueller_spieler] += 2
-        meldung = 'Du hast eine 2 geworfen!'
-    elif karte == 3:
-        counter[aktueller_spieler] += 3
-        meldung = 'Du hast eine 3 geworfen!'
-    elif karte == 4:
-        random.shuffle(felder)
-        meldung = 'Du hast eine 4 geworfen!'
-    elif karte == 5:
-        meldung = 'Shoty! Du hast eine 5 geworfen!'
-
-    session['counter'] = counter
-    session['felder'] = felder
-    aktueller_spieler = (aktueller_spieler + 1) % len(spieler_namen)
-    session['aktueller_spieler'] = aktueller_spieler
+    runde()  # ⬅️ Spiellogik aufrufen
 
     return render_template('spiel.html',
-                           spieler_namen=spieler_namen,
-                           counter=counter,
-                           meldung=meldung,
-                           aktueller=spieler_namen[aktueller_spieler])
+                           spieler_namen=session['spieler_namen'],
+                           counter=session['counter'],
+                           meldung=session['meldung'],
+                           aktueller=session['spieler_namen'][session['aktueller_spieler']],
+                           gezogene_karte=session['gezogene_karte'],
+                           zip=zip)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
